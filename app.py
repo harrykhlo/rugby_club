034fe4f70839247d9c5b4462a4031043976fe96e
Harry Lo <harrykhlo@gmail.com>
2021-10-16 13:17:09 +1300

    create login page at login.html

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 # Student Name: Harry
 # Student Id: 1149599
 
+
+# python -m venv env
+
+# python -m pip install --upgrade pip
+
+# python -m pip install flask
+
+# pip install mysql-connector-python
+
 # python -m flask run
 
 from flask import Flask
@@ -38,11 +47,56 @@ def getID():
 
 @app.route("/")
 def home():
-    # cur = getCursor()
-    # # cur.execute("select id, company, last_name from customers;")
-    # cur.execute("select MemberID, MemberFirstName, MemberLastName from members;")
-    # select_result = cur.fetchall()
-    # column_name = [decs[0] for decs in cur.description]
-    # print(f"{column_name}")
-    # return render_template('customerresult.html', dbresult=select_result, dbcols=column_name)
-    return render_template('login.html')
+    cur = getCursor()
+    # cur.execute("select id, company, last_name from customers;")
+    cur.execute("select MemberID, MemberFirstName, MemberLastName from members;")
+    select_result = cur.fetchall()
+    column_name = [decs[0] for decs in cur.description]
+    print(f"{column_name}")
+    return render_template('listmembernames.html', dbresult=select_result, dbcols=column_name)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # print(request.form) # for checing only
+
+        # get member ID from the login from in login.html
+        memberid = request.form.get('selectedmember')
+        # print(memberid) # for checing only
+
+        if memberid == "None":  # if no member is selected from the login from in login.html
+            print("Not member is given")
+            cur = getCursor()
+            cur.execute(
+                "select MemberID, MemberFirstName, MemberLastName from members;")
+            select_result = cur.fetchall()
+            return render_template('login.html', membernameidlist=select_result)
+        else:  # if a member is selected from the login from in login.html
+            cur = getCursor()
+            # get the member details
+            cur.execute(
+                "select * from members where MemberID = %s;", (memberid,))
+            memberrecord = cur.fetchall()
+            # memberrecorddiscription = cur.description # for checking only
+            # print(memberrecorddiscription) # for checking only
+
+            # get the member's AdminAccess which is an integer (1 = admin member; 0 = general member)
+            adminaccess = memberrecord[0][12]
+            if adminaccess:  # check if the memeber is admin or not
+                # route to admin's page
+                print("admin is logged in")
+                print(memberrecord)
+                return render_template('adminindex.html', admindetaillist=memberrecord[0])
+            else:  # if the memeber is not an admin
+                # route to member's page
+                print("member is logged in")
+                print(memberrecord)
+                return render_template('memberindex.html', memberdetaillist=memberrecord[0])
+    # if request.method == 'GET':
+    else:
+        cur = getCursor()
+        cur.execute(
+            "select MemberID, MemberFirstName, MemberLastName from members;")
+        select_result = cur.fetchall()
+        return render_template('login.html', membernameidlist=select_result)
